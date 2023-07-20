2e3e6f97c9889be7ed2d6a7e0a63fbb692aec44b
Gabriela <gabriela2santtanna@gmail.com>
2023-07-20 13:19:00 -0300

    Primeiros passo em Python

---
+++ Variáveis_e_constantes.py
@@ -0,0 +1,8 @@
+age, name = (18, 'Gabi')
+print(f'Meu nome é {name} e eu tenho {age} ano(s) de idade.')
+
+age, name = (20, 'Alguém')
+print(f'Meu nome é {name} e eu tenho {age} ano(s) de idade.')
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+print(BRAZILIAN_STATES)
---
+++ conversao_de_tipos.py
@@ -0,0 +1,39 @@
+#Int para Float: 
+preco = 10
+print(preco)
+
+preco = float(preco)
+print(preco)
+
+preco = 10 / 2
+print(preco)
+
+#De float para Int
+preco = 10.30
+print(preco)
+preco = int(preco)
+print(preco)
+
+preco = 10
+print(preco)
+print(preco // 2)
+
+#Num > string
+preco = 150.50
+idade = 100
+
+print(str(preco))
+print(str(idade))
+
+texto = f"idade {idade} preco {preco}"
+print(texto)
+
+#string > num
+
+preco = 150.50
+idade = 100
+print(float(preco))
+print(int(idade))
+
+#Tipo: 
+print(type(str(10.10)))
